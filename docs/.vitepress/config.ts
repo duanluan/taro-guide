ca06824f3da283f4bfe505ff6febc6a14d5f838e
duanluan <duanluan@outlook.com>
2023-07-22 19:27:10 +0800

    feat: 新增介绍、环境准备

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,36 @@
+import {defineConfig} from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: "/taro-guide/",
+  // 左上角标题
+  title: "Taro 渐进式教程",
+  description: "Vue3（组合式 API）+ TypeScript + Sass + Pinia 的图书管理系统",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      {text: '首页', link: '/'},
+      {text: '文档', link: '/介绍'}
+    ],
+
+    sidebar: [
+      {
+        text: '开始',
+        items: [
+          {text: '介绍', link: '/介绍'},
+          {text: '环境准备', link: '/环境准备'}
+        ],
+      },
+      {
+        text: '快速上手',
+        items: [
+          {text: '初始化项目', link: '/初始化项目'},
+        ]
+      }
+    ],
+
+    socialLinks: [
+      {icon: 'github', link: 'https://github.com/vuejs/vitepress'}
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Taro 渐进式教程"
+  text: "图书管理系统"
+  tagline: Vue3（组合式 API）+ TypeScript + Sass + Pinia 
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /介绍
+    - theme: alt
+      text: GitHub
+      link: https://github.com/duanluan/taro-guide
+
+features:
+  - title: 简单易学
+    details: 以前端初学者的思路带你入门，注释、引用详细。
+  - title: 精确可用
+    details: 不会错过每一个文件，保证你复制粘贴之后就能运行看到效果。
+  - title: 无需后端
+    details: 使用 JSON Server 搭建后端服务，提供接口。
+---
+
